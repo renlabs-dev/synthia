36ad4310e5e6701e4d6ee7525eccef2650a397ad
supreme <zimulajan0@gmail.com>
2024-03-24 18:47:58 +0100

    WIP

--- src/synthia/validator/validator.py
+++ src/synthia/validator/validator.py
@@ -5,6 +5,6 @@ import asyncio
 if __name__ == "__main__":
     from communex.compat.key import classic_load_key # type: ignore
     keypair = classic_load_key("gay-person")
-    client = ModuleClient("localhost", 1300, keypair)
+    client = ModuleClient("localhost", 8000, keypair)
     result = asyncio.run(client.request("method/generate", {"prompt": "abc"}))
     print(result)
