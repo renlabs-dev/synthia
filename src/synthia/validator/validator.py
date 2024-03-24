37846e7324c252cff3bcb6f9c6be2f63ba69aed2
Kelvin Steiner <me@steinerkelvin.dev>
2024-03-24 17:18:12 -0300

    small fix

--- src/synthia/validator/validator.py
+++ src/synthia/validator/validator.py
@@ -3,8 +3,7 @@ from communex.module.client import ModuleClient # type: ignore
 import asyncio
 
 if __name__ == "__main__":
-    from communex.compat.key import classic_load_key # type: ignore
     keypair = classic_load_key("gay-person")
     client = ModuleClient("localhost", 8000, keypair)
-    result = asyncio.run(client.request("method/generate", {"prompt": "abc"}))
+    result = asyncio.run(client.call("generate", {"prompt": "abc"}))
     print(result)
