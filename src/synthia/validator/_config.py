f332f7ab9005a285f51eeb938f0b74a8b54d3b40
supreme <zimulajan0@gmail.com>
2024-03-25 23:13:39 +0100

    Update Synthia validator configuration and initialize Wandb run

--- src/synthia/validator/_config.py
+++ src/synthia/validator/_config.py
@@ -1,15 +1,21 @@
 from pydantic_settings import BaseSettings
 
+
 class ValidatorSettings(BaseSettings):
-    api_key: str # openai key
+    api_key: str  # openai key, used to generate questions and answers
+    # place holders, in case user uses the same env file to run the validator and the miner
+    model: str | None = None
+    temperature: int | None = None
+    max_tokens: int | None = None
+    # wandb
     project_name: str = "synthia"
+    storage_path: str = "/synthia/db"
     # currently only limited to openai
     question_model: str = "gpt-3.5-turbo"
     answer_model: str = "gpt-4"
+    question_temperature: float = 0.5
+    answer_temperature: float = 0.1
 
     class Config:
         env_prefix = "OPENAI_"
-        env_file = "env/config.env"
-
-
-    
+        env_file = "env/openai.env"
--- src/synthia/validator/text_validator.py
+++ src/synthia/validator/text_validator.py
@@ -1,36 +1,66 @@
+"""
+Module for validating text submissions on the Synthia subnet.
+
+This class represents a validator module that runs on the Synthia subnet. It initializes a Wandb run to log validation data, and provides functions to retrieve metadata about the subnet and modules.
+"""
+
+# once we register the synthia subnet, we will update this value
 from communex.module.module import Module  # type: ignore
 from communex.client import CommuneClient  # type: ignore
 from substrateinterface import Keypair  # type: ignore
-from communex.module._signer import sign  # type: ignore
 from communex.module.client import serialize  # type: ignore
+from communex.compat.types import Ss58Address  # type: ignore
+from typing import cast
 import time
 from ._config import ValidatorSettings
 import wandb
 
+SYNTHIA_NETUID = 1
+
+# TODO:
+# Jairo
+# - implement retry mechanism on question answer generation
+# - make sure miner instructions are the same as answer generation of validator. WIth the same system instructions and tempreature
+# - implement the main validation loop -> get question, get answer, loop through miners, ...  (without validation that is done by kelvin)
+# - query the `SYNTHIA_NETUID` dynamically from the chain name of the subnet
+
+# Honza
+# figure out saving to db
+# figure out a better prompt
+# making more robust, think about it
+
+# Kelvin
+# Implement scoring function based on vector difference using embedings
+
 
 class TextValidator(Module):
-    def __init__(self) -> None:
+    def __init__(self, key: Keypair, netuid: int) -> None:
         super().__init__()
         self.node_url = "wss://commune.api.onfinality.io/public-ws"
         self.client = CommuneClient(url=self.node_url)
+        self.key = key
+        self.netuid = netuid
 
-    def get_synthia_netuid(self, client: CommuneClient):
+    def get_synthia_netuid(self):
         """
         Retrives the netuid of the synthia subnet
         """
-        pass
+        return self.netuid
 
-    def get_modules(self, client: CommuneClient):
+    def get_modules(self, client: CommuneClient, netuid: int) -> list[str]:
         """
-        Retrives all modules from the subnet
+        Retrives all module addresses from the subnet
         """
-        pass
+        module_addreses = client.query_map_address(netuid).values()
+        return list(module_addreses)
 
-    def init_wandb(
-        self, clinet: CommuneClient, settings: ValidatorSettings, keypair: Keypair
-    ):
+    # TODO :
+    # - Migrate from wandb to decentralized database, possibly ipfs, the server
+    # has to check for the signature of the data. (This way is used even on S18, but we don't like it)
+    def init_wandb(self, settings: ValidatorSettings, keypair: Keypair):
         # place holder, query later
-        uid = 0
+        key = cast(Ss58Address, keypair.ss58_address)
+        uid = self.client.get_uids(key=key)
         run_name = f"validator-{uid}"
         settings.uid = uid
         settings.key = keypair.ss58_address
@@ -39,17 +69,18 @@ class TextValidator(Module):
         settings.timestamp = time.time()
         config = settings.model_dump()
         signature = keypair.sign(serialize(config))
+        config["signature"] = signature
         # Initialize the wandb run for the single project
         run = wandb.init(  #  type: ignore
             name=run_name,
             project=settings.project_name,
             entity="synthia-subnet",
             config=config,
-            dir=config.full_path,
+            dir=settings.storage_path,
             reinit=True,
         )
 
-        # Sign the run to ensure it's from the correct hotkey
-        signature = keypair.sign(run.id.encode()).hex()  #  type: ignore
-        config.signature = signature
-        wandb.config.update(config, allow_val_change=True)
+    def main(self, settings: ValidatorSettings | None = None) -> None:
+        if not settings:
+            settings = ValidatorSettings()  # type: ignore
+        self.init_wandb(settings, self.key)
