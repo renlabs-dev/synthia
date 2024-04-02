0c17459ae3d257518173e74e7104614b31344783
PsicoThePato <jmomcontato@gmail.com>
2024-04-02 11:15:57 -0300

    :recycle: :adhesive_bandage: fix: correctly prompting

--- src/synthia/miner/anthropic.py
+++ src/synthia/miner/anthropic.py
@@ -1,54 +1,89 @@
+from typing import Any
+
 from fastapi import HTTPException
 from anthropic import Anthropic, APIError
 from communex.module.module import Module, endpoint  # type: ignore
+from anthropic._types import NotGiven
+
 from ._config import AnthropicSettings  # Import the AnthropicSettings class from config
-from typing import Any
+from ..validator.meta_prompt import explanation_prompt
+from .BaseLLM import BaseLLM
 
-
-class AnthropicModule(Module):
+class AnthropicModule(Module, BaseLLM):
     def __init__(self, settings: AnthropicSettings | None = None) -> None:
         super().__init__()
         self.settings = settings or AnthropicSettings()  # type: ignore
         self.client = Anthropic(api_key=self.settings.api_key)
-
+        self.system_prompt = (
+            "You are a supreme polymath renowned for your ability to explain"
+            "complex concepts effectively to any audience from laypeople"
+            "to fellow top experts. "
+            "By principle, you always ensure factual accuracy."
+            "You are master at adapting your explanation strategy as needed" 
+            "based on the topic and target audience, using a wide array of"
+            "tools such as examples, analogies and metaphors whenever and"
+            "only when appropriate. Your goal is their comprehension of the"
+            "explanation, according to their background expertise."
+            "You always structure your explanations coherently and express"
+            "yourself clear and concisely, crystallizing thoughts and"
+            "key concepts. You only respond with the explanations themselves," 
+            "eliminating redundant conversational additions."
+            f"Try to keep your answer below {self.settings.max_tokens} tokens"
+            )
+        
     @endpoint
     def generate(self, prompt: str) -> dict[str, str]:
         try:
-            system_prompt = (
-                "You are an expert explanation generator. "
-                "Generate an explanation based on the given instructions. "
-                "Provide the output solely in the specified JSON format, without any extra text or deviations."
-            )
-            assistant_prompt = (
-                "Output the answer strictly in the following JSON format:\n"
-                '{"explanation": "replace this with your actual explanation."}\n'
-                "Provide the output solely in the specified JSON format, without any extra text or deviations."
-            )
-            message = self.client.messages.create(
-                model=self.settings.model,
-                max_tokens=self.settings.max_tokens,
-                temperature=self.settings.temperature,
-                system=system_prompt,
-                messages=[
-                    {"role": "user", "content": [{"type": "text", "text": prompt}]},
-                    {
-                        "role": "assistant",
-                        "content": [{"type": "text", "text": assistant_prompt}],
-                    },
-                ],
-            )
-            return self._treat_response(message)
+            message = self.prompt(prompt, self.system_prompt)
         except APIError as e:
             raise HTTPException(status_code=e.status_code, detail=str(e)) from e  # type: ignore
+        
+        match message:
+            case None, explanation:
+                raise HTTPException(status_code=500, detail=explanation)
+            case answer, _:
+                return {"answer": answer} 
 
-    def _treat_response(self, message: Any) -> dict[str, str]:
-        content = message.messages[-1].content.strip()
-        try:
-            json_start = content.index("{")
-            json_end = content.rindex("}")
-            json_content = content[json_start : json_end + 1]
-            return {"answer": json_content}
-        except (ValueError, IndexError):
-            raise HTTPException(
-                status_code=400, detail="Invalid JSON format in the response"
-            )
+    def prompt(
+            self, user_prompt: str, 
+            system_prompt: str | None | NotGiven = None
+        ):
+        if not system_prompt:
+            system_prompt = NotGiven()
+        message = self.client.messages.create(
+        model=self.settings.model,
+        max_tokens=self.settings.max_tokens,
+        temperature=self.settings.temperature,
+        system=system_prompt,
+        messages=[
+            {"role": "user", "content": user_prompt},
+        ],
+        
+    )
+        treated_message = self._treat_response(message)
+        return treated_message
+    
+    def _treat_response(self, message: Any):
+        # TODO: use result ADT
+        if message.dict()["stop_sequence"] is not None:
+            return None, "Max tokens were not enough to generate an answer"
+        
+        blocks = message.dict()["content"]
+        answer = "".join([block["text"] for block in blocks])
+        return answer, ""
+
+    @property
+    def max_tokens(self) -> int:
+        return self.settings.max_tokens
+
+if __name__ == "__main__":
+    from communex.module.server import ModuleServer
+    from substrateinterface import Keypair
+
+    import uvicorn
+    KEY_MNEMONIC = "electric suffer nephew rough gentle decline fun body tray account vital clinic"
+    key = Keypair.create_from_mnemonic(KEY_MNEMONIC)
+    claude = AnthropicModule()
+    server = ModuleServer(claude, key)
+    app = server.get_fastapi_app()
+    uvicorn.run(app, host="127.0.0.1", port=8000)
